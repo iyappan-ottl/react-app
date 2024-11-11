38994f795a0fbf2791e6d6c67ce225005feb1cd5
Iyappan Samiraj <iyappan.samiraj@openturf.in>
2024-11-11 11:28:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ function App() {
             <li>
               <Link to="/contact">Contact</Link>
             </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
         </nav>
 
