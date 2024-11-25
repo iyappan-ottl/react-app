e02f4ac0c08cf7d7357b87a43730d056b46b65a5
Iyappan Samiraj <iyappan.samiraj@openturf.in>
2024-11-25 19:14:08 +0530

    Develop changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './components/home';
-import About from './About';
-import Contact from './components/Contact';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [form, setForm] = useState({ name: '', description: '', price: '' });
+
+  // Fetch items from backend
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/items')
+      .then(res => setItems(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    axios.post('http://localhost:3001/api/items', form)
+      .then(res => setItems([...items, res.data]))
+      .catch(err => console.error(err));
+  };
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:3001/api/items/${id}`)
+      .then(() => setItems(items.filter(item => item._id !== id)))
+      .catch(err => console.error(err));
+  };
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+    <div>
+      <h1>CRUD App</h1>
+      <form>
+        <input name="name" placeholder="Name" onChange={handleChange} />
+        <input name="description" placeholder="Description" onChange={handleChange} />
+        <input name="price" type="number" placeholder="Price" onChange={handleChange} />
+        <button type="button" onClick={handleSubmit}>Add Item</button>
+      </form>
 
-        <Routes>
-          <Route path="/home" exact element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+      <ul>
+        {items.map(item => (
+          <li key={item._id}>
+            {item.name} - {item.description} - ${item.price}
+            <button onClick={() => handleDelete(item._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default App;
